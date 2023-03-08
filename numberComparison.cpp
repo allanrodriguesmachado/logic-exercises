5afabf5efecf5619a4956a494bbe254fc39258e5
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-03-08 17:04:08 -0300

    :up:

---
+++ numberComparison.cpp
@@ -0,0 +1,47 @@
+/*
+    Program that asks for two numbers and prints the larger one.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    printf("Bem vindo!\n\n");
+
+    int variableA, variableB;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%i", &variableA);
+
+    printf("Digite outro numero inteiro: ");
+    scanf("%i", &variableB);
+ 
+    printf("\n");
+
+    if (variableA == variableB) {
+        printf("Atencao! Operacao invalida\n");
+        printf("Os numero sao iguais\n");
+        printf("\n");
+        return system("pause");
+    }
+
+    if (variableA > variableB)
+    { 
+        printf("A - e o maior numero: %i", variableA);
+        printf("\n");
+        return system("pause \n");
+      
+    }
+    
+    if (!(variableA > variableB))
+    {
+        printf("B - e o maior numero: %i", variableB);
+        printf("\n");
+        return  system("pause \n");
+    }
+
+    system("pause");
+
+    return 0;
+}
